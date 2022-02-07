e8521c870a38a05276cfc82844faa8f72b3077f5
Suresh Kumamr <choudharysureshk128@gmail.com>
2022-02-07 11:18:21 +0530

    07/02/2022

--- Tree/BSTcreation.cpp
+++ Tree/BSTcreation.cpp
@@ -40,7 +40,6 @@ Node* remove(Node* root,int key){
     }
     else{
         //when current node matches with the key
-
         //0 children
         if(root->left==NULL&&root->right==NULL){
             delete root;
@@ -65,7 +64,6 @@ Node* remove(Node* root,int key){
             root->key=temp->key;
             root->right=remove(root->right,temp->key);
         }
-
     }
     return root;
 }
@@ -81,6 +79,22 @@ bool searchBST(Node* root,int key){
         return searchBST(root->right,key);
     }
 }
+
+//print range
+void printRange(Node* root,int k1,int k2){
+    if(root==NULL)return ;
+    if(root->key>=k1 and root->key<=k2){
+        printRange(root->left,k1,k2);
+        cout<<root->key<<" ";
+        printRange(root->right,k1,k2);
+    }
+    else if(root->key<k1){
+        printRange(root->right,k1,k2);
+    }
+    else if(root->key>k2){
+        printRange(root->left,k1,k2);
+    }
+}
 //inorder print
 void printBst(Node* root){
 if(root==NULL)return ;
@@ -89,6 +103,23 @@ cout<<root->key<<"  ";
 printBst(root->right);
 }
 
+//root to leaf == all the paths 
+void root2leaf(Node* root,vector<int>&v){
+if(root==NULL)return;
+if(root->left==NULL and root->right==NULL){
+    v.push_back(root->key);
+    for(int i:v){
+        cout<<i<<"->";
+    }
+    cout<<endl;
+    v.pop_back();
+    }
+     v.push_back(root->key);
+     root2leaf(root->left,v);
+     root2leaf(root->right,v);
+     v.pop_back();
+ return ;
+}
 int main(){
 Node* root =NULL;
 int arr[]={8,3,10,1,6,14,4,7,13};
@@ -99,9 +130,13 @@ printBst(root);
 cout<<endl;
 // if(searchBST(root,13))cout<<"present "<<endl;
 // else cout<<"not present"<<endl;
-int key;
-cin>>key;
-root=remove(root,key);
-printBst(root);
+// int key;
+// cin>>key;
+// root=remove(root,key);
+// printBst(root);
+//cout<<"range print is "<<endl;
+//printRange(root,3,13);
+vector<int>v;
+root2leaf(root,v);
 return 0;
 }
